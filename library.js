d8d684ecacc0373e50de12c16aec4b47174242d6
Omar Ahmad <superomar121@gmail.com>
2022-01-11 14:02:40 +0400

    adding book works; need to rectify empty textbox

--- library.js
+++ library.js
@@ -1,37 +1,8 @@
-let myLibrary = [
-    {
+let myLibrary = [{
         title: 'Harry Potter and the Goblet of Fire',
         author: 'JK Rolling',
         pages: '111',
         read: true,
-    },
-
-    {
-        title: '1',
-        author: '#1',
-        pages: '111',
-        read: true,
-    },
-
-    {
-        title: '1',
-        author: '#1',
-        pages: '111',
-        read: false,
-    },
-
-    {
-        title: '1',
-        author: '#1',
-        pages: '111',
-        read: false,
-    },
-
-    {
-        title: '1',
-        author: '#1',
-        pages: '111',
-        read: false,
     },
 ];
 
@@ -62,13 +33,10 @@ function changeReadStatus(book) {
     book.read = !book.read;
 }
 
-function toggleFormPopUp() {
-    const form = document.querySelector('nav form');
-    // form.style.display = 'block';
-}
-
 function displayLibrary(library, displayContainer) {
     if (library.length !== 0) {
+        // Clear all elements in container
+        booksListDiv.textContent = '';
         library.forEach(book => {
             const newBookDiv = document.createElement('div');
             newBookDiv.className = 'book';
@@ -85,46 +53,63 @@ function displayLibrary(library, displayContainer) {
                 if (header === 'read') {
                     element = document.createElement('button');
                     element.classList.add('readButton');
-                    if (book.read)    element.classList.add('read');
-                    else    element.classList.add('notRead');
+                    if (book.read) element.classList.add('read');
+                    else element.classList.add('notRead');
                 } else {
                     element = document.createElement('h3');
                 }
                 element.innerText = headers[header];
                 newBookDiv.appendChild(element);
-                
+
             }
 
             displayContainer.appendChild(newBookDiv);
+            
 
         });
-    }       
+        addEventListenersToReadButtons();
+    }
+}
+
+function addEventListenersToReadButtons() {
+    // Read Buttons
+    const readButton = document.querySelectorAll('.readButton');
+    readButton.forEach((button, key) => {
+        button.addEventListener('click', () => {
+            // Clear classes
+            button.classList.remove('read');
+            button.classList.remove('notRead');
+
+            // Update book value
+            let myBook = myLibrary[key];
+            myBook.read = !myBook.read;
+
+            // Change View of button
+            if (myBook.read) {
+                button.classList.add('read');
+                button.innerText = readButtonText.read;
+            } else {
+                button.classList.add('notRead');
+                button.innerText = readButtonText.notRead;
+            }
+        });
+    })
 }
 
 displayLibrary(myLibrary, booksListDiv);
 
 // New Book Button
 const newBookButton = document.querySelector('#newBookButton');
-newBookButton.addEventListener('click', toggleFormPopUp);
-// Read Buttons
-const readButton = document.querySelectorAll('.readButton');
-readButton.forEach((button, key) => {
-    button.addEventListener('click', () => {
-        // Clear classes
-        button.classList.remove('read');
-        button.classList.remove('notRead');
-        
-        // Update book value
-        let myBook = myLibrary[key];
-        myBook.read = !myBook.read;
-        
-        // Change View of button
-        if (myBook.read) {
-            button.classList.add('read');
-            button.innerText = readButtonText.read;
-        } else {
-            button.classList.add('notRead');
-            button.innerText = readButtonText.notRead;
+const formInputs = document.querySelectorAll('nav form div input')
+newBookButton.addEventListener('click', () => {
+    let newBook = new Book();
+    formInputs.forEach((input, index) => {
+        if (input.type === 'checkbox') newBook.read = input.checked
+        else {
+            newBook[index] = input.value;
         }
-    });
-})
+    })
+    addBookToLibrary(myLibrary, newBook);
+    displayLibrary(myLibrary, booksListDiv);
+
+});
